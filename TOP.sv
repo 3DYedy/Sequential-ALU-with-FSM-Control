6ed3d6e817fca83b27ebf6bc637bf2557f971ea5
3DYedy <e_palade@yahoo.com>
2026-02-24 19:16:12 +0200

    TOP.sv

--- TOP.sv
+++ TOP.sv
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 02/18/2026 03:14:33 AM
-// Design Name: 
-// Module Name: TOP
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 module TOP(
     input  logic clk,
