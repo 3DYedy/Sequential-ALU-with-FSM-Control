48fecb164665aa508f2b32cb7b6ab4a9da021899
3DYedy <e_palade@yahoo.com>
2026-02-24 19:16:28 +0200

    TOP_TB.sv

--- TOP_TB.sv
+++ TOP_TB.sv
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 02/18/2026 03:15:19 AM
-// Design Name: 
-// Module Name: TOP_TB
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 module TOP_TB();
 
